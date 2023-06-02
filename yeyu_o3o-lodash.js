48f9f36f9323db5d9af5ae86d0d414b6042c08df
yeyuo3o <727117718@qq.com>
2023-06-02 22:32:36 +0800

    6.2

--- yeyu_o3o-lodash.js
+++ yeyu_o3o-lodash.js
@@ -1,17 +1,34 @@
 var yeyu_o3o = {
+
   compact: function (array) {
     var res = []
     for (var i = 0; i < array.length; i++) {
-      if (array[i] == false || array[i] == null || array[i] == 0 || array[i] == '' || array[i] == undefined || array[i] == NaN) {
-
-      } else {
+      if (array[i]) {
         res.push(array[i])
       }
     }
-    return array
-  }
+    return res
+  },
 
+  chunk: function (array, [size = 1]) {
+    var res = []
+    for (var i = 0; i < array.length; i += size) {
+      res.push(array.slice(i, i + size))
+    }
+    return res
+  },
+
+  fill: function (array, value, [start = 0], [end = array.length]) {
+    for (var i = start; i < end; i++) {
+      array[i] = value
+    }
+    return array
+  },
+
+  drop: function (array, [n = 1]) {
+    return array.slice(0, n)
+  }
 }
 
-
+// http://10.3.3.3:33312/statics/lodash/
 // https://www.lodashjs.com/
